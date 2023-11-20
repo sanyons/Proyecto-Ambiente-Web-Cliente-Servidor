eb74666d4e79de001d391d362553026a470b755c
Jose Daniel Acuna Rojas <jdanielcomp@hotmail.com>
2023-11-19 21:46:03 -0600

    Adding list

--- categoria.php
+++ categoria.php
@@ -15,7 +15,52 @@
         <h1>Listado categoría</h1>
         <!-- Aqui listado mascota -->
         
-
+        <section th:fragment="listadoCategorias" id="categorias">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="card">
+                            <div class="card-header" style="background-color: #42829C;">
+                                <h4 style="color:white">[[#{categoria.listado}]]</h4>
+                            </div>
+                            <div th:if="${categorias != null and !categorias.empty}">
+                                <table class="table table-striped table-hover">
+                                    <thead class="table-primary">
+                                        <tr><th>#</th>
+                                            <th>[[#{categoria.descripcion}]]</th>
+                                            <th>[[#{categoria.activo}]]</th>
+                                            <th sec:authorize ="hasRole('ROLE_ADMIN')"></th></tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr th:each="categoria, contador : ${categorias}">
+                                            <td>[[${contador.count}]]</td>
+                                            <td>[[${categoria.descripcion}]]</td>
+                                            <td th:text="${categoria.activo} ? 'Activa' : 'Inactiva'" />
+                                            <td sec:authorize ="hasRole('ROLE_ADMIN')"><a th:href="@{/categoria/eliminar/}+${categoria.idCategoria}"
+                                                   class="btn btn-danger">
+                                                    <i class="fas fa-trash"></i> [[#{accion.eliminar}]]</a>
+                                                <a th:href="@{/categoria/modificar/}+${categoria.idCategoria}"
+                                                   class="btn btn-success">
+                                                    <i class="fas fa-pencil"></i> [[#{accion.actualizar}]]</a></td></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="text-center p-2" th:if="${categorias == null or categorias.empty}">
+                                <span>[[#{lista.vacia}]]</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card text-center mb-3">
+                        <img src=https://static.vecteezy.com/system/resources/previews/004/568/669/non_2x/category-line-icon-vector.jpg alt="categorias" style="width:100%">
+                        <div class="container">
+                        <h4>[[#{plantilla.categorias}]]</h4>
+                        </div>
+                     </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 
 
--- usuario.php
+++ usuario.php
@@ -15,7 +15,54 @@
         <h1>Listado usuario</h1>
         <!-- Aqui listado mascota -->
         
-
+        <section th:fragment="listadoUsuarios" id="usuarios">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="card">
+                            <div class="card-header"style="background-color: #42829C;">
+                                <h4 style="color:white">[[#{usuario.listado}]]</h4></div>
+                            <div th:if="${usuarios != null and !usuarios.empty}">
+                                <table class="table table-striped table-hover">
+                                    <thead class="table-primary">
+                                        <tr><th>#</th>
+                                            <th>[[#{usuario.username}]]</th>
+                                            <th>[[#{usuario.nombre}]]</th>
+                                            <th>[[#{usuario.apellidos}]]</th>
+                                            <th></th></tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr th:each="usuario, contador : ${usuarios}">
+                                            <td>[[${contador.count}]]</td>
+                                            <td>[[${usuario.username}]]</td>
+                                            <td>[[${usuario.nombre}]]</td>
+                                            <td>[[${usuario.apellidos}]]</td>
+                                            <td sec:authorize="hasRole('ROLE_ADMIN')">
+                                                <a th:href="@{/usuario/eliminar/}+${usuario.idUsuario}"
+                                                   class="btn btn-danger">
+                                                    <i class="fas fa-trash"></i> [[#{accion.eliminar}]]</a>
+                                                <a th:href="@{/usuario/modificar/}+${usuario.idUsuario}"
+                                                   class="btn btn-success">
+                                                    <i class="fas fa-pencil"></i> [[#{accion.actualizar}]]</a></td></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="text-center p-2" th:if="${usuarios == null or usuarios.empty}">
+                                <span>[[#{lista.vacia}]]</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card text-center mb-3">
+                        <img src=https://cdn-icons-png.flaticon.com/512/1077/1077063.png alt="categorias" style="width:100%">
+                        <div class="container">
+                        <h4 class="fs-2">[[#{plantilla.usuarios}]]</h4>
+                        </div>
+                     </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 
 
